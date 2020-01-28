96fde9d1a5557e13e26724e75dd83d9f3654c183
triciacodes <tleach@gmail.com>
2020-01-27 16:55:22 -0800

    add Form component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import { css } from 'emotion'
 
 import { mq } from './utils/media-queries'
 
+import Form from './components/Form'
+
 const styles = {
   root: css(
     mq({
-      margin: ['25px', '50px', '100px'],
+      backgroundColor: '#eee',
+      minHeight: '100%',
+      padding: ['25px', '50px', '100px'],
     }),
   ),
 }
 
 function App() {
-  return <div className={styles.root}></div>
+  return (
+    <div className={styles.root}>
+      <Form />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { css } from 'emotion'
+
+const styles = {
+  form: css({
+    maxWidth: '200px',
+    margin: '40px auto',
+    background: 'white',
+    padding: '10px',
+  }),
+  input: css({
+    display: 'block',
+    margin: '10px auto',
+    padding: '4px',
+    border: '1px solid #aaa',
+  }),
+}
+
+const handleSubmit = e => {
+  e.preventDefault()
+  console.log(e, 'this is event')
+}
+
+const Form = () => {
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <input
+        className={styles.input}
+        id="username"
+        placeholder="username"
+        type="text"
+      />
+      <input className={styles.input} type="submit" value="Submit" />
+    </form>
+  )
+}
+
+export default Form
